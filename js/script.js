dee687bd6ed6c1971ed48ec33f013d375cd07199
Dom-888 <spinelladomenico88@gmail.com>
2020-01-25 13:16:08 +0000

    Complete challenge

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 
+$(document).ready(function() {
+    $(".stream-nav").click(function() {
+        $(".card").removeClass("card-highlight");
+        var streamNumber = $(this).attr("ID");
+        $("." + streamNumber + "-card").addClass("card-highlight");
+    });
+});
+
